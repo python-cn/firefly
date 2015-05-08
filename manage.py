6c76c3d2a9c24b877dbd79a2094d663991e831aa
moon2l <moon2l@foxmail.com>
2015-05-08 18:09:44 +0800

    add shell command, you are able to debug the app use 'python manage.py shell'

--- manage.py
+++ manage.py
@@ -1,11 +1,22 @@
 # coding=utf-8
 
-from flask_script import Manager, Server
+from flask_script import Manager, Server, Shell
 
 from firefly.app import create_app
 
+from firefly.models.user import Role, User, SocialConnection
+
+
+def make_shell_context():
+    return {
+        "Role": Role,
+        "User": User,
+        "SocialConnection": SocialConnection
+    }
+
 manager = Manager(create_app)
 manager.add_option('-c', '--config', dest='config', required=False)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('runserver', Server(
     use_debugger=True,
     use_reloader=True,
