d6af0e7dd4cef1da690b6d9e04191fd69bfea105
killpanda <angus@killpanda.de>
2015-09-09 18:59:21 +0800

    add user setting form and view class

--- firefly/views/user.py
+++ firefly/views/user.py
@@ -1,8 +1,13 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+from flask import url_for, redirect
+
 from flask.views import MethodView
 from flask.blueprints import Blueprint
+from flask.ext.login import login_required, current_user
+from firefly.libs.template import render_template
+from firefly.forms.user import ProfileForm
 
 
 bp = Blueprint('user', __name__, url_prefix='/user')
@@ -12,4 +17,17 @@ class UserView(MethodView):
     def get(self, id):
         return ''
 
+
+class UserSettingsView(MethodView):
+    decorators = [login_required]
+
+    def get(self):
+        return render_template('user/settings.html', user=current_user)
+
+    def post(self):
+        form = ProfileForm()
+        form.save()
+        return redirect(url_for('user.settings'))
+
 bp.add_url_rule('/<id>/', view_func=UserView.as_view('detail'))
+bp.add_url_rule('/settings', view_func=UserSettingsView.as_view('settings'))
